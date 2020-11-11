cc61bceb062a00ac093ca02a665fe904047c05d2
Brenton Bongcaron <brenton3926@gmail.com>
2020-11-11 13:34:31 -0500

    Moved bulk of main() to new makeProgram(). Updated comments.

--- hw5/hw5/interpret.c
+++ hw5/hw5/interpret.c
@@ -2,9 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include "instruction.h"
-
+/*
+ *  @resetTemps   resets temporary variables used in building program[]
+ */
 void resetTemps(char* action, char* arg1, char* arg2, char* arg3);
-
+/*
+ *  @makeProgram  builds program[] based off .asm file
+ */
+int makeProgram(FILE* asm, Instruction* program[]);
+/*
+ *  @freeProgram  frees all malloc'ed potions of program[]
+ */
 void freeProgram(Instruction* program[], int endOfProgram);
 
 int main(int argc, char* argv[])
@@ -13,17 +21,36 @@ int main(int argc, char* argv[])
   Instruction* program[101];
   /* Pointer to the current line in the assembly program */
   //int currentLine = 1;
-  /* Pointer to the end of the assembly program */
-  int endOfProgram = 1;
+  /* Assembly file to be scanned */
   FILE* asm = fopen(argv[1], "r");
 
+  /* Pointer to the end of the assembly program */
+  int endOfProgram = makeProgram(asm, program);
+
+  /* Free all malloc'ed portions of program */
+  freeProgram(program, endOfProgram);
+
+}
+
+void resetTemps(char* action, char* arg1, char* arg2, char* arg3)
+{
+  strcpy(action, "\0");
+  strcpy(arg1, "\0");
+  strcpy(arg2, "\0");
+  strcpy(arg3, "\0");
+  return;
+}
+
+int makeProgram(FILE* asm, Instruction* program[])
+{
   /* Declaration of temporary placeholders of assembly strings */
   char line[100] = "\0";
   char action[6] = "\0";
   char arg1[6] = "\0";
   char arg2[6] = "\0";
   char arg3[6] = "\0";
-
+  /* Pointer to end of program */
+  int endOfProgram = 1;
   /* Go through each line of the .asm file */
   while(fgets(line, 100, asm) != NULL)
   {
@@ -60,22 +87,12 @@ int main(int argc, char* argv[])
     createInstruction(&i, action, arg1, arg2, arg3);
     /* Add Instruction i to program[endOfProgram] */
     program[endOfProgram] = i;
+    /* Update endOfProgram */
     endOfProgram++;
     /* Reset temporary variables */
     resetTemps(action, arg1, arg2, arg3);
   }
-
-  freeProgram(program, endOfProgram);
-
-}
-
-void resetTemps(char* action, char* arg1, char* arg2, char* arg3)
-{
-  strcpy(action, "\0");
-  strcpy(arg1, "\0");
-  strcpy(arg2, "\0");
-  strcpy(arg3, "\0");
-  return;
+  return endOfProgram;
 }
 
 void freeProgram(Instruction* program[], int endOfProgram)
