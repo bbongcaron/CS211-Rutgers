7e2b556af7fc2ceb3a9a2f6ac6463066f0d1a1bf
Brenton Bongcaron <brenton3926@gmail.com>
2020-12-05 17:40:13 -0500

    hw7: Built runCircuit(), passed 7/10 test cases, temps up next

--- hw7/hw7/first.c
+++ hw7/hw7/first.c
@@ -13,11 +13,14 @@ typedef struct Gate
   struct Gate* next;
 } Gate;
 
-Gate* buildCircuit(FILE* circuit_file);
-int** buildTruthTable(int numInputs, int numOutputs);
 void freeCircuit(Gate* circuit);
-void freeMatrix(void** matrix, int numRows);
+void freeCharMatrix(char** matrix, int numRows);
+void freeIntMatrix(int** matrix, int numRows);
+int** buildTruthTable(int numInputs, int numOutputs);
 void printTruthTable(int** truthTable, int numRows, int numCols);
+Gate* buildCircuit(FILE* circuit_file);
+void runCircuit(Gate* circuit, int** truthTable, char** inVars, char** outVars,
+                int numInputs, int numOutputs);
 
 int main(int argc, char* argv[argc+1])
 {
@@ -30,36 +33,36 @@ int main(int argc, char* argv[argc+1])
   /* scan INPUTVARS */
   fscanf(circuit_file, "%*s %d", &numInputs);
   inVars = malloc(sizeof(char*)*numInputs);
-  printf("INPUTVAR %d", numInputs);
+  //printf("INPUTVAR %d", numInputs);
   for (int i = 0; i < numInputs; i++) {
     inVars[i] = malloc(sizeof(char)*100);
     fscanf(circuit_file, " %s", inVars[i]);
-    printf(" %s", inVars[i]);
+    //printf(" %s", inVars[i]);
   }
-  printf("\n");
+  //printf("\n");
   /* scan OUTPUTVARS */
   fscanf(circuit_file, "%*s %d", &numOutputs);
   outVars = malloc(sizeof(char*)*numOutputs);
-  printf("OUTPUTVAR %d", numOutputs);
+  //printf("OUTPUTVAR %d", numOutputs);
   for (int i = 0; i < numOutputs; i++) {
     outVars[i] = malloc(sizeof(char)*100);
     fscanf(circuit_file, " %s", outVars[i]);
-    printf(" %s", outVars[i]);
+    //printf(" %s", outVars[i]);
   }
-  printf("\n");
+  //printf("\n");
   /* scan circuit instructions */
   circuit = buildCircuit(circuit_file);
   /* Initialize output truth table */
   int** truthTable = buildTruthTable(numInputs, numOutputs);
 
   // need code here to calculate outputs
-
+  runCircuit(circuit, truthTable, inVars, outVars, numInputs, numOutputs);
   /* Print out resulting truth table */
   printTruthTable(truthTable, (int)(pow(2, numInputs)), numInputs + numOutputs);
   /* Free all malloc'ed stuff */
-  freeMatrix(inVars, numInputs);
-  freeMatrix(outVars, numOutputs);
-  freeMatrix(truthTable, (int)(pow(2, numInputs)));
+  freeCharMatrix(inVars, numInputs);
+  freeCharMatrix(outVars, numOutputs);
+  freeIntMatrix(truthTable, (int)(pow(2, numInputs)));
   freeCircuit(circuit);
   fclose(circuit_file);
   return EXIT_SUCCESS;
@@ -78,7 +81,17 @@ void printTruthTable(int** truthTable, int numRows, int numCols)
   }
 }
 
-void freeMatrix(void** matrix, int numRows)
+void freeCharMatrix(char** matrix, int numRows)
+{
+  /* Free double array */
+  for (int i = 0; i < numRows; i++)
+  {
+    free(matrix[i]);
+  }
+  free(matrix);
+}
+
+void freeIntMatrix(int** matrix, int numRows)
 {
   /* Free double array */
   for (int i = 0; i < numRows; i++)
@@ -137,14 +150,15 @@ Gate* createGate(FILE* circuit_file, char* operation)
   strcpy(newGate->operation, operation);
   /* Scan in arguments and output variables to fields of newGate */
   fscanf(circuit_file, "%s", newGate->arg1);
-  fscanf(circuit_file, "%s", newGate->arg2);
+  if (strcmp(operation, "NOT") != 0) fscanf(circuit_file, "%s", newGate->arg2);
   fscanf(circuit_file, "%s", newGate->outputVar);
   newGate->next = NULL;
-  /* Debug | Output newGate fields */
+  /* Debug | Output newGate fields
   printf("%s ", operation);
   printf("%s ", newGate->arg1);
-  printf("%s ", newGate->arg2);
+  if (strcmp(operation, "NOT") != 0) printf("%s ", newGate->arg2);
   printf("%s\n", newGate->outputVar);
+  */
   return newGate;
 }
 
@@ -176,4 +190,104 @@ Gate* buildCircuit(FILE* circuit_file)
     }
   }
   return circuit;
+}
+
+typedef struct Variable{
+  char name[100];
+  int value;
+} Variable;
+
+void runCircuit(Gate* circuit, int** truthTable, char** inVars, char** outVars,
+                int numInputs, int numOutputs)
+{
+  int numRows = (int)(pow(2, numInputs));
+  for (int i = 0; i < numRows; i++)
+  {
+    /* Malloc 1D arrays of Variable structs: contains names & boolean values */
+    Variable* inputs = malloc(sizeof(Variable)*numInputs);
+    Variable* outputs = malloc(sizeof(Variable)*numOutputs);
+    /* Assign truth values based on pre-made truth table input rows */
+    for (int j = 0; j < numInputs; j++)
+    {
+      /* Assign input Variable names & values based on inVars & truthTable */
+      strcpy(inputs[j].name, inVars[j]);
+      inputs[j].value = truthTable[i][j];
+    }
+    for (int j = 0; j < numOutputs; j++)
+    {
+      /* Assign output Variable names based in outVars */
+      strcpy(outputs[j].name, outVars[j]);
+    }
+    /* Initialize gate pointer */
+    Gate* currentGate = circuit;
+    while (currentGate)
+    {
+      int inputOneIndex = 0; int inputTwoIndex = 0;
+      bool foundOne = false;
+      bool foundTwo;
+      if (strcmp(currentGate->operation, "NOT") != 0) foundTwo = false;
+      else foundTwo = true;
+      int outputIndex = 0; bool foundOut = false;
+      /* Find indexes of Gate inputs in Variable arrays to pull boolean values */
+      while (!foundOne || !foundTwo ||!foundOut)
+      {
+        if (!foundOne)
+        {
+          if (strcmp(inputs[inputOneIndex].name, currentGate->arg1) == 0)
+            foundOne = true;
+          else inputOneIndex++;
+        }
+        if (!foundTwo)
+        {
+          if (strcmp(inputs[inputTwoIndex].name, currentGate->arg2) == 0)
+            foundTwo = true;
+          else inputTwoIndex++;
+        }
+        if (!foundOut)
+        {
+          if (strcmp(outputs[outputIndex].name, currentGate->outputVar) == 0)
+            foundOut = true;
+          else outputIndex++;
+        }
+      }
+      if (strcmp(currentGate->operation, "AND") == 0)
+      {
+        outputs[outputIndex].value = inputs[inputOneIndex].value
+                                                && inputs[inputTwoIndex].value;
+      }
+      else if (strcmp(currentGate->operation, "OR") == 0)
+      {
+        outputs[outputIndex].value = inputs[inputOneIndex].value
+                                                || inputs[inputTwoIndex].value;
+      }
+      else if (strcmp(currentGate->operation, "NAND") == 0)
+      {
+        outputs[outputIndex].value = !(inputs[inputOneIndex].value
+                                                && inputs[inputTwoIndex].value);
+      }
+      else if (strcmp(currentGate->operation, "NOR") == 0)
+      {
+        outputs[outputIndex].value = !(inputs[inputOneIndex].value
+                                                || inputs[inputTwoIndex].value);
+      }
+      else if (strcmp(currentGate->operation, "NOT") == 0)
+      {
+        outputs[outputIndex].value = !(inputs[inputOneIndex].value);
+      }
+      else if (strcmp(currentGate->operation, "XOR") == 0)
+      {
+        outputs[outputIndex].value = (inputs[inputOneIndex].value
+                                    || inputs[inputTwoIndex].value)
+                                  && !(inputs[inputOneIndex].value
+                                    && inputs[inputTwoIndex].value);
+      }
+      /* Fill in solved outputs in the corresponding truthTable row */
+      for (int j = 0; j < numOutputs; j++)
+        truthTable[i][numInputs + j] = outputs[j].value;
+
+      currentGate = currentGate->next;
+    }
+    free(inputs);
+    free(outputs);
+  }
 }
