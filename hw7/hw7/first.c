88cc7cf9457365273bcb19f1c754a0dee6e0839b
Brenton Bongcaron <brenton3926@gmail.com>
2020-12-05 15:31:30 -0500

    hw7: Read in & store remainder of circuit file instructions in LL

--- hw7/hw7/first.c
+++ hw7/hw7/first.c
@@ -4,6 +4,7 @@
 
 typedef struct Gate
 {
+  /* a Gate strcut to build the circuit with */
   char operation[5];
   char arg1[100];
   char arg2[100];
@@ -11,8 +12,51 @@ typedef struct Gate
   struct Gate* next;
 } Gate;
 
+Gate* buildCircuit(FILE* circuit_file);
+void freeCircuit(Gate* circuit);
+void freeMatrix(char** matrix, int numElements);
+
+int main(int argc, char* argv[argc+1])
+{
+  /* Open circuit file */
+  FILE* circuit_file = fopen(argv[1], "r");
+  /* Initialize/declare necessary variables */
+  char** inVars; char** outVars;
+  int numInputs = 0; int numOutputs = 0;
+  Gate* circuit = NULL;
+  /* scan INPUTVARS */
+  fscanf(circuit_file, "%*s %d", &numInputs);
+  inVars = malloc(sizeof(char*)*numInputs);
+  printf("INPUTVAR %d", numInputs);
+  for (int i = 0; i < numInputs; i++) {
+    inVars[i] = malloc(sizeof(char)*100);
+    fscanf(circuit_file, " %s", inVars[i]);
+    printf(" %s", inVars[i]);
+  }
+  printf("\n");
+  /* scan OUTPUTVARS */
+  fscanf(circuit_file, "%*s %d", &numOutputs);
+  outVars = malloc(sizeof(char*)*numOutputs);
+  printf("OUTPUTVAR %d", numOutputs);
+  for (int i = 0; i < numOutputs; i++) {
+    outVars[i] = malloc(sizeof(char)*100);
+    fscanf(circuit_file, " %s", outVars[i]);
+    printf(" %s", outVars[i]);
+  }
+  printf("\n");
+  /* scan circuit instructions */
+  circuit = buildCircuit(circuit_file);
+  /* Free all malloc'ed stuff */
+  freeMatrix(inVars, numInputs);
+  freeMatrix(outVars, numOutputs);
+  freeCircuit(circuit);
+  fclose(circuit_file);
+  return EXIT_SUCCESS;
+}
+
 void freeMatrix(char** matrix, int numElements)
 {
+  /* Free matrix of char strings */
   for (int i = 0; i < numElements; i++)
   {
     free(matrix[i]);
@@ -20,35 +64,62 @@ void freeMatrix(char** matrix, int numElements)
   free(matrix);
 }
 
-int main(int argc, char* argv[argc+1])
+void freeCircuit(Gate* circuit)
 {
-  FILE* circuit_file = fopen(argv[1], "r");
-  char** inVars; char** outVars;
-  int numInputs = 0; int numOutputs = 0;
-  Gate* circuit = NULL;
+  /* Free circuit linked list */
+  Gate* ptr = circuit;
+  Gate* prev = NULL;
+  while (ptr)
+  {
+    prev = ptr;
+    ptr = ptr->next;
+    free(prev);
+  }
+}
 
-  /* scan INPUTVARS */
-  fscanf(circuit_file, "%*s %d", &numInputs);
-  inVars = malloc(sizeof(char*)*numInputs);
-  //printf("INPUTVAR");
-  for (int i = 0; i < numInputs; i++) {
-    inVars[i] = malloc(sizeof(char)*100);
-    fscanf(circuit_file, " %s", inVars[i]);
-    //printf(" %s", inVars[i]);
+Gate* createGate(FILE* circuit_file, char* operation)
+{
+  Gate* newGate = malloc(sizeof(Gate));
+  strcpy(newGate->operation, operation);
+  /* Scan in arguments and output variables to fields of newGate */
+  fscanf(circuit_file, "%s", newGate->arg1);
+  fscanf(circuit_file, "%s", newGate->arg2);
+  fscanf(circuit_file, "%s", newGate->outputVar);
+  newGate->next = NULL;
+  /* Debug | Output newGate fields */
+  printf("%s ", operation);
+  printf("%s ", newGate->arg1);
+  printf("%s ", newGate->arg2);
+  printf("%s\n", newGate->outputVar);
+  return newGate;
+}
+
+Gate* buildCircuit(FILE* circuit_file)
+{
+  Gate* circuit = NULL;
+  Gate* newGate = NULL;
+  Gate* lastGate = circuit;
+  char operation[10] = "\0";
+  while (fscanf(circuit_file, "%s ", operation) != EOF)
+  {
+    // Malloc a newGate corresponding to operation for circuit
+    newGate = createGate(circuit_file, operation);
+
+    if (!circuit && !lastGate)
+    {
+      // Check: Returned circuit is empty
+      // Action 1: Initialize circuit when first Gate is created
+      circuit = newGate;
+      // Action 2: When first Gate is created, set the lastGate to be that Gate
+      lastGate = circuit;
+    }
+    else // The circuit already has at least 1 Gate
+    {
+      // Append newGate to the end of circuit (lastGate)
+      lastGate->next = newGate;
+      // Update the circuit's lastGate pointer
+      lastGate = lastGate->next;
+    }
   }
-  //printf("\n");
-  /* scan OUTPUTVARS */
-  fscanf(circuit_file, "%*s %d", &numOutputs);
-  outVars = malloc(sizeof(char*)*numOutputs);
-  //printf("OUTPUTVAR");
-  for (int i = 0; i < numOutputs; i++) {
-    outVars[i] = malloc(sizeof(char)*100);
-    fscanf(circuit_file, " %s", outVars[i]);
-    //printf(" %s", outVars[i]);
-  }
-  //printf("\n");
-  freeMatrix(inVars, numInputs);
-  freeMatrix(outVars, numOutputs);
-  fclose(circuit_file);
-  return EXIT_SUCCESS;
+  return circuit;
 }
