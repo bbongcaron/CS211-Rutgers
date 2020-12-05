5d5f0f788553a84eabf8800e610cbf524c9d232a
Brenton Bongcaron <brenton3926@gmail.com>
2020-12-05 15:12:21 -0500

    hw7: Created Gate struct, read INPUTVARS & OUTPUTVARS without while loop

--- hw7/hw7/first.c
+++ hw7/hw7/first.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Gate
+{
+  char operation[5];
+  char arg1[100];
+  char arg2[100];
+  char outputVar[100];
+  struct Gate* next;
+} Gate;
+
 void freeMatrix(char** matrix, int numElements)
 {
   for (int i = 0; i < numElements; i++)
@@ -14,30 +23,30 @@ void freeMatrix(char** matrix, int numElements)
 int main(int argc, char* argv[argc+1])
 {
   FILE* circuit_file = fopen(argv[1], "r");
-  char action[10] = "\0";
   char** inVars; char** outVars;
   int numInputs = 0; int numOutputs = 0;
-  while (fscanf(circuit_file, "%s", action) != EOF)
-  {
-    if (strcmp(action, "INPUTVAR") == 0)
-    {
-      fscanf(circuit_file, " %d", &numInputs);
-      inVars = malloc(sizeof(char*)*numInputs);
-      for (int i = 0; i < numInputs; i++) {
-        inVars[i] = malloc(sizeof(char)*100);
-        fscanf(circuit_file, " %s", inVars[i]);
-      }
-    }
-    else if (strcmp(action, "OUTPUTVAR") == 0)
-    {
-      fscanf(circuit_file, " %d", &numOutputs);
-      outVars = malloc(sizeof(char*)*numOutputs);
-      for (int i = 0; i < numOutputs; i++) {
-        outVars[i] = malloc(sizeof(char)*100);
-        fscanf(circuit_file, " %s", outVars[i]);
-      }
-    }
+  Gate* circuit = NULL;
+
+  /* scan INPUTVARS */
+  fscanf(circuit_file, "%*s %d", &numInputs);
+  inVars = malloc(sizeof(char*)*numInputs);
+  //printf("INPUTVAR");
+  for (int i = 0; i < numInputs; i++) {
+    inVars[i] = malloc(sizeof(char)*100);
+    fscanf(circuit_file, " %s", inVars[i]);
+    //printf(" %s", inVars[i]);
   }
+  //printf("\n");
+  /* scan OUTPUTVARS */
+  fscanf(circuit_file, "%*s %d", &numOutputs);
+  outVars = malloc(sizeof(char*)*numOutputs);
+  //printf("OUTPUTVAR");
+  for (int i = 0; i < numOutputs; i++) {
+    outVars[i] = malloc(sizeof(char)*100);
+    fscanf(circuit_file, " %s", outVars[i]);
+    //printf(" %s", outVars[i]);
+  }
+  //printf("\n");
   freeMatrix(inVars, numInputs);
   freeMatrix(outVars, numOutputs);
   fclose(circuit_file);
